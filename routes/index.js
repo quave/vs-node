91d91ce70690dd8d51a09e3589db395303dbc77d
Quave <vladsynkov@gmail.com>
2015-10-09 14:41:56 +0000

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,26 @@
-
 /*
  * GET home page.
  */
 
+var request = require('request');
+var fs = require('fs');
+var url = require('url');
+
+var filename = 'file';
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  var urlStr = req.param('url');
+
+  if (urlStr == undefined || !urlStr) {
+    res.render('index', { title: 'Express' });
+    return;
+  }
+
+  var urlObj = url.parse(urlStr);
+  
+  request(urlObj.href)
+  .on('end', function () {
+    res.download(filename);
+  })
+  .pipe(fs.createWriteStream(filename));
 };
