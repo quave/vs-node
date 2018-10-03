19834ebed25e19dab7c05684f9b974f7a9557f4f
Vlad Synkov <himself@vladsynkov.ru>
2018-10-03 18:47:44 +0300

    Unknown WIP

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
+const koa = require('koa');
+const koarouter = require('koa-router');
+const json = require('koa-json');
 
-/**
- * Module dependencies.
- */
+const router = new koarouter();
 
-var express = require('express');
+router.get('/', async ctx => {
+  console.log('Rouuter', '/', arguments);
+
+  ctx.body = {message: 'Yala yala!'};
+});
+
+const app = new koa();
+app.use(json());
+app.use(router.routes());
+app.listen(3001);
+console.log('Listening on 3001');
+
+/*
 var routes = require('./routes');
 var user = require('./routes/user');
-var http = require('http');
 var path = require('path');
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 2001);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -20,7 +31,6 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
@@ -31,6 +41,4 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+*/
---
+++ ui/src/components/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{header}}</h1>
+  <
+</template>
+
+<script>
+const data = {};
+
+this.$http.get('')
+
+export default {
+  name: 'hello',
+  data: () => data
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
